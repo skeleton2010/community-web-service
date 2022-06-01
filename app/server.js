913eec14c8ee9433e8a65bd70def2380a1f4b049
skeleton2010 <inseong747@gmail.com>
2022-06-01 09:31:48 +0900

    프런트단에서 작동하는 jsfile 생성

--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ app.set('views', "./src/views");
 app.set('view engine', 'ejs');
 
 const home = require('./src/routes/home/index');
+app.use(express.static((`${__dirname}/src/public`)))
 app.use("/", home);
 
 module.exports = app;
