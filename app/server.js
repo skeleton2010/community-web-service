cd0387e6981adc03796c39a219666e2d33670135
skeleton2010 <inseong747@gmail.com>
2022-06-02 09:27:58 +0900

    login API구현

--- app/server.js
+++ app/server.js
@@ -9,7 +9,10 @@ app.set('views', "./src/views");
 app.set('view engine', 'ejs');
 
 const home = require('./src/routes/home/index');
-app.use(express.static((`${__dirname}/src/public`)))
+app.use(express.static((`${__dirname}/src/public`)));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
 app.use("/", home);
 
 module.exports = app;
